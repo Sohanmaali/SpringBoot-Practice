4152523ccd56691719050a232742d7136dd508a0
Sohan Maali <sohanmaali4@gmail.com>
2024-04-12 02:10:09 +0530

    file add

---
+++ SpringBoot-First/src/main/java/com/first/SpringBootFirstApplication.java
@@ -0,0 +1,15 @@
+package com.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstApplication.class, args);
+		
+		System.out.println("I am Spring boot");
+	}
+
+}
---
+++ SpringBoot-web/src/main/java/com/web/entities/User.java
@@ -0,0 +1,57 @@
+package com.web.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String address;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
